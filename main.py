696d82c0abe1ee092365e4b7ee9bb7bb818ce00c
ImGajeed76 <imgajeed.76@gmail.com>
2022-02-08 12:32:42 +0100

    Update

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import censurer
 
-c_words_path = "censure_words.txt"
-e_words_path = "exceptions.txt"
+if __name__ == '__main__':
+    c_words_path = "censure_words.txt"
+    e_words_path = "exceptions.txt"
 
-c = censurer.Censure()
-c.set_censure_words(c.delete_duplicates(c_words_path))
-c.set_exceptions(c.delete_duplicates(e_words_path))
+    c = censurer.Censure()
+    c.set_censure_words(c.delete_duplicates(c_words_path))
+    c.set_exceptions(c.delete_duplicates(e_words_path))
 
-c.update("")
-print(c.raw_string)
-print(c.censured_string)
+    while True:
+        print("To exit type :stop:")
+        text = input("Some text: ")
+        if text == ":stop:":
+            break
+        c.update(text)
+        print("Not Censured: " + c.raw_string)
+        print("Censured" + c.censured_string)
 
-c.save_censures(c_words_path)
-c.save_exceptions(e_words_path)
+    c.save_censures(c_words_path)
+    c.save_exceptions(e_words_path)
