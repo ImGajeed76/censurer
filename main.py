64aab0a39e8a54ed1354721a0147a2e3d982a98d
ImGajeed76 <imgajeed.76@gmail.com>
2022-02-08 12:37:52 +0100

    Update

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ if __name__ == '__main__':
     c = censurer.Censure()
     c.set_censure_words(c.delete_duplicates(c_words_path))
     c.set_exceptions(c.delete_duplicates(e_words_path))
+    print("\n" * 100)
 
     while True:
-        print("To exit type :stop:")
+        print("To exit type -> :stop:")
         text = input("Some text: ")
         if text == ":stop:":
             break
         c.update(text)
-        print("Not Censured: " + c.raw_string)
-        print("Censured" + c.censured_string)
+        print("Not Censured: \t" + c.raw_string)
+        print("Censured: \t\t" + c.censured_string)
+        input()
+        print("\n"*100)
 
     c.save_censures(c_words_path)
     c.save_exceptions(e_words_path)
